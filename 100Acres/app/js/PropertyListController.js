7c35bda7873805f33c307d3c4764401c69e801df
Faiz Mohammed <faiz.mohammed@outlook.com>
2013-09-15 23:47:47 +0530

    New Project 100acres created
    New Project 100acres created

---
+++ 100Acres/app/js/PropertyListController.js
@@ -0,0 +1,56 @@
+﻿
+function propertyListCtrl($scope, Property) {
+//    $http.get('app/listings/listings.json').success(function (data) {
+//        $scope.properties = data;
+//    }).error(function (data, status) {
+//        alert('error ' + status);
+    //    });
+    Property.newListings().then(function (response) {
+        $scope.allListings = response;
+        //$scope.properties = response;
+        $scope.bigTotalItems = response.length;
+        loadDataForCurrentPage(0);
+    });
+    //$scope.allListings = Property.newListings();
+    //$scope.properties = Property.newListings();
+
+    //Pagination settings
+    //$scope.bigTotalItems = $scope.properties.length;
+    $scope.currentPage = 4;
+    $scope.maxSize = 5;
+    $scope.bigCurrentPage = 1;
+    $scope.itemsPerPage = 10;
+    $scope.setPage = function (pageNo) {
+        $scope.bigCurrentPage = pageNo;
+        loadDataForCurrentPage(pageNo-1);
+    };
+
+    var loadDataForCurrentPage = function (currentPageNo) {
+        var itemsPerPage = $scope.itemsPerPage;
+        var totalItems = $scope.totalItems;
+        var startIndex = (itemsPerPage * currentPageNo);
+        var endIndex = startIndex + itemsPerPage;
+
+        if (startIndex > 0) {
+            startIndex = startIndex + 1;
+        }
+
+        $scope.properties = $scope.allListings.slice(startIndex, endIndex);
+    }
+
+    //$scope.$watch('bigCurrentPage', loadDataForCurrentPage(bigCurrentPage), false);
+
+}
+
+//function loadDataForCurrentPage($scope, currentPageNo) {
+//    var itemsPerPage = $sccope.itemsPerPage;
+//    var totalItems = $scope.totalItems;
+//    var startIndex = (itemsPerPage * currentPageNo);
+//    var endIndex = startIndex + itemsPerPage;
+
+//    if (startIndex > 0) {
+//        startIndex = startIndex + 1;
+//    }
+
+//    $scope.properties = $scope.allListings.slice(startIndex, endIndex);
+//}
---
+++ 100Acres/app/js/app.js
@@ -0,0 +1,33 @@
+﻿
+var app = angular.module('100acres', ['ngResource', 'ui.bootstrap.pagination']);
+
+app.factory('Property', function ($q, $http) {
+    //return $resource('app/listings/listings.json', {}, { newListings: { method: 'GET', cache: true, isArray: true} });
+    return {
+        newListings: function () {
+            var deferred = $q.defer();
+            $http.get('app/listings/listings.json').success(function (data) {
+                deferred.resolve(data);
+            }).error(function (data, status) {
+                deferred.reject();
+            });
+
+            return deferred.promise;
+        }
+    }
+});
+
+app.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/properties', {templateUrl:'app/partials/property_listings.htm', controller:propertyListCtrl}).
+    otherwise({redirectTo:'/properties'});
+} ]);
+
+//app.directive('pagination', function () {
+//    return {
+//        restrict: 'A',
+//        link: function (scope, element, attrs) {
+//            $('div.holder').jPages({containerID:'listingContainer', perPage: scope.$eval(attrs.pagination).pageSize});
+//    }
+//};
+//});
+
