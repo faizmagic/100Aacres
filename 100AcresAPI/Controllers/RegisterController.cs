5c4e1d1b729e5dbfb89d4b0b3d2af3fc96c724af
Faiz Mohammed <faiz.mohammed@outlook.com>
2013-10-20 19:06:51 +0530

    updated API project to point to local IIS
    updated API project to point to local IIS

--- 100AcresAPI/Controllers/RegisterController.cs
+++ 100AcresAPI/Controllers/RegisterController.cs
@@ -13,17 +13,16 @@ namespace _100AcresAPI.Controllers
 {
     public class RegisterController : ApiController
     {
-        //public HttpResponseMessage PostRegisterUser(User user)
-        public HttpResponseMessage PostRegisterUser()
+        public HttpResponseMessage PostRegisterUser(User user)
         {
             HttpResponseMessage response = null;
             try
             {
                 //Save the user data to DB
                 //if success set the cookie and authenticate the user
-                //FormsAuthentication.SetAuthCookie(user.UserName, true);
+                FormsAuthentication.SetAuthCookie(user.UserName, true);
                 //Membership.ValidateUser(
-                response = Request.CreateResponse(HttpStatusCode.OK);
+                response = Request.CreateResponse(HttpStatusCode.OK, user);
                 response.Headers.Add("Access-Control-Allow-Origin", "*");
             }
             catch (Exception ex)
